4dd2f4d694e7ff51fc901339892fc223288bb182
Mike Joyce <michael.joyce@target.com>
2019-05-02 16:18:02 -0500

    tell ts to shutup

--- src/messageListener.ts
+++ src/messageListener.ts
@@ -20,65 +20,65 @@ import { ComponentData } from './type'
 // )
 
 export function subscribeToHighlight() {
-  function listener(evt: any) {
-    if (evt.data) {
-      const message = JSON.parse(evt.data) as any
+	function listener(evt: any) {
+		if (evt.data) {
+			const message = JSON.parse(evt.data) as any
 
-      if (message == null || message.type !== 'highlight') return
+			if (message == null || message.type !== 'highlight') return
 
-      const { action, selector } = message
+			const { action, selector } = message
 
-      if (action === 'focus') {
-        addShader(selector)
-      } else {
-        removeShader()
-      }
-    }
-  }
+			if (action === 'focus') {
+				addShader(selector)
+			} else {
+				removeShader()
+			}
+		}
+	}
 
-  const shadingDiv = document.createElement('div') as any
-  shadingDiv.classList.add('shader')
-  document.body.appendChild(shadingDiv)
+	const shadingDiv = document.createElement('div') as any
+	shadingDiv.classList.add('shader')
+	document.body.appendChild(shadingDiv)
 
-  function addShader(selector: string) {
-    const s = `[data-highlightid=${selector}]`
-    const el = document.querySelector(s)
+	function addShader(selector: string) {
+		const s = `[data-highlightid=${selector}]`
+		const el = document.querySelector(s)
 
-    const { left, top, width, height } = el.getBoundingClientRect()
+		const { left, top, width, height } = el!.getBoundingClientRect()
 
-    const styleString = `left: ${left}px; top: ${top}px; width: ${width}px; height: ${height}px;`
+		const styleString = `left: ${left}px; top: ${top}px; width: ${width}px; height: ${height}px;`
 
-    shadingDiv.style = styleString
-  }
+		shadingDiv.style = styleString
+	}
 
-  function removeShader() {
-    shadingDiv.style = ''
-  }
+	function removeShader() {
+		shadingDiv.style = ''
+	}
 
-  window.addEventListener('message', listener)
+	window.addEventListener('message', listener)
 
-  return function unsub() {
-    window.removeEventListener('message', listener)
-  }
+	return function unsub() {
+		window.removeEventListener('message', listener)
+	}
 }
 
 export function subscribeToComponentUpdate(cb: (data: ComponentData) => void) {
-  function messageListener(evt: any) {
-    // change this to data once we're all set
-    if (evt.data) {
-      const message = JSON.parse(evt.data) as any
+	function messageListener(evt: any) {
+		// change this to data once we're all set
+		if (evt.data) {
+			const message = JSON.parse(evt.data) as any
 
-      if (message == null || message.type !== 'ce') return
+			if (message == null || message.type !== 'ce') return
 
-      const componentData = message.componentData as ComponentData
+			const componentData = message.componentData as ComponentData
 
-      cb(componentData)
-    }
-  }
+			cb(componentData)
+		}
+	}
 
-  window.addEventListener('message', messageListener)
+	window.addEventListener('message', messageListener)
 
-  return function unsubscribe() {
-    window.removeEventListener('message', messageListener)
-  }
+	return function unsubscribe() {
+		window.removeEventListener('message', messageListener)
+	}
 }
